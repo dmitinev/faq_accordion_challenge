0fce12f42b7263f7a01d3a02dd6528d25d4adfc9
Dmitry Mitinev <83422156+dmitinev@users.noreply.github.com>
2024-03-17 11:28:35 +0100

    initial

---
+++ src/js/app.js
@@ -0,0 +1,6 @@
+import {testWebP as isWebPSupported} from './modules/testWebp.js';
+
+isWebPSupported.then((result) => {
+  const className = result ? 'webp' : 'no-webp';
+  document.body.classList.add(className);
+});
